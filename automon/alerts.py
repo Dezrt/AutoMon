92af73b790760fa33b5d2d79cdcbe0b58fd4e279
Dezrt <63778795+Dezrt@users.noreply.github.com>
2025-02-10 19:20:12 +0300

    Update alerts.py

--- automon/alerts.py
+++ automon/alerts.py
@@ -1,7 +1,7 @@
 import requests
 
-TOKEN = "7721562129:AAGCPT2hB1Nn3wPJOHrvhl-uxgV6fXGWVz8"
-CHAT_ID = "7721562129"
+TOKEN = ""
+CHAT_ID = ""
 
 def send_alert(message):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
